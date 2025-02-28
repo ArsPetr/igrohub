7e27e187cf3c8240a8811544ab32b975642deedd
ArsPetr <arseniyp1@yandex.ru>
2025-02-28 17:19:08 +0300

    Заменил Input Manager на Imput Actions

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,14 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using static NewControls;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IPlayerActions
 {
     private Rigidbody2D rigidbody2D;
     private Vector2 movement;
 
     public float speed = 100f;
+
+    private NewControls inputActions;
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+
+        if (inputActions == null )
+        {
+            inputActions = new NewControls();
+            inputActions.Player.SetCallbacks( this );
+        }
+        
+        inputActions.Enable();
     }
 
     private void Update()
@@ -18,7 +30,14 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        //movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
         rigidbody2D.linearVelocity = movement * speed;
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector3>();
+        movement.Normalize();
+        Debug.Log($"Movement: {movement}");
     }
 }
